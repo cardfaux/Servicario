2a61ee9580bda777b8163830f28f46a22e6104fd
James Hagood <cardfaux@tutanota.com>
2020-02-09 13:32:42 -0600

    added hero component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import NavbarClone from '../components/NavbarClone';
+import Hero from '../components/Hero';
 import ServiceItem from '../components/service/ServiceItem';
 
 import { getServices } from '../store';
@@ -30,36 +31,7 @@ class Home extends React.Component {
 			<div>
 				<Navbar />
 				<NavbarClone />
-				<section className='hero is-default is-bold'>
-					<div className='hero-body'>
-						<div className='container'>
-							<div className='columns is-vcentered'>
-								<div className='column is-5 is-offset-1 landing-caption'>
-									<h1 className='title is-1 is-bold is-spaced'>
-										Learn, Collaborate.
-									</h1>
-									<h2 className='subtitle is-5 is-muted'>
-										Lorem ipsum sit dolor amet is a dummy text used by
-										typography industry{' '}
-									</h2>
-									<p>
-										<a className='button cta rounded primary-btn raised'>
-											Get Started
-										</a>
-									</p>
-								</div>
-								<div className='column is-5 is-offset-1'>
-									<figure className='image is-4by3'>
-										<img
-											src={process.env.PUBLIC_URL + '/worker.svg'}
-											alt='Description'
-										/>
-									</figure>
-								</div>
-							</div>
-						</div>
-					</div>
-				</section>
+				<Hero />
 
 				<section className='section section-feature-grey is-medium'>
 					<div className='container'>
