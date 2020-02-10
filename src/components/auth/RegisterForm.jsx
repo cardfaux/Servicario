3e1344b652a642a121ace500a84c0688aaecf7b0
James Hagood <cardfaux@tutanota.com>
2020-02-10 17:56:04 -0600

    added register form

---
+++ src/components/auth/RegisterForm.jsx
@@ -0,0 +1,148 @@
+/* eslint-disable no-useless-escape */
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+const RegisterForm = () => {
+	const { register, handleSubmit, errors } = useForm();
+
+	const getFormData = (data) => {
+		console.log(data);
+	};
+
+	return (
+		<form onSubmit={handleSubmit(getFormData)}>
+			<div className='field'>
+				<div className='control'>
+					<input
+						ref={register({
+							required: true,
+							pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+						})}
+						name='email'
+						className='input is-large'
+						type='email'
+						placeholder='Your Email'
+						autoComplete='email'
+					/>
+					{errors.email && (
+						<div className='form-error'>
+							{errors.email.type === 'required' && (
+								<span className='help is-danger'>Email is required</span>
+							)}
+							{errors.email.type === 'pattern' && (
+								<span className='help is-danger'>
+									Email address is not valid
+								</span>
+							)}
+						</div>
+					)}
+				</div>
+			</div>
+			<div className='field'>
+				<div className='control'>
+					<input
+						ref={register({ required: true, minLength: 10 })}
+						name='fullName'
+						className='input is-large'
+						type='text'
+						placeholder='Full Name'
+					/>
+					{errors.fullName && (
+						<div className='form-error'>
+							{errors.fullName.type === 'required' && (
+								<span className='help is-danger'>Name is required</span>
+							)}
+							{errors.fullName.type === 'minLength' && (
+								<span className='help is-danger'>
+									Minimum Length Is 10 Characters
+								</span>
+							)}
+						</div>
+					)}
+				</div>
+			</div>
+			<div className='field'>
+				<div className='control'>
+					<input
+						ref={register({
+							required: true,
+							pattern: /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm
+						})}
+						name='avatar'
+						className='input is-large'
+						type='text'
+						placeholder='Avatar'
+					/>
+					{errors.avatar && (
+						<div className='form-error'>
+							{errors.avatar.type === 'required' && (
+								<span className='help is-danger'>Avatar is required</span>
+							)}
+							{errors.avatar.type === 'pattern' && (
+								<span className='help is-danger'>Avatart url is not valid</span>
+							)}
+						</div>
+					)}
+				</div>
+			</div>
+			<div className='field'>
+				<div className='control'>
+					<input
+						ref={register({ required: true, minLength: 6 })}
+						name='password'
+						className='input is-large'
+						type='password'
+						placeholder='Your Password'
+						autoComplete='current-password'
+					/>
+					{errors.password && (
+						<div className='form-error'>
+							{errors.password.type === 'required' && (
+								<span className='help is-danger'>Password is required</span>
+							)}
+							{errors.password.type === 'minLength' && (
+								<span className='help is-danger'>
+									Minimum Length Is 6 Characters
+								</span>
+							)}
+						</div>
+					)}
+				</div>
+			</div>
+			<div className='field'>
+				<div className='control'>
+					<input
+						ref={register({ required: true, minLength: 6 })}
+						name='passwordConfirmation'
+						className='input is-large'
+						type='password'
+						placeholder='Repeat Password'
+						autoComplete='current-password'
+					/>
+					{errors.passwordConfirmation && (
+						<div className='form-error'>
+							{errors.password.type === 'required' && (
+								<span className='help is-danger'>
+									Password confirmation is required
+								</span>
+							)}
+							{errors.password.type === 'minLength' && (
+								<span className='help is-danger'>
+									Minimum Length Is 6 Characters
+								</span>
+							)}
+						</div>
+					)}
+				</div>
+			</div>
+			<button
+				type='submit'
+				className='button is-block is-info is-large is-fullwidth'
+			>
+				Register
+			</button>
+		</form>
+	);
+};
+
+export default RegisterForm;
