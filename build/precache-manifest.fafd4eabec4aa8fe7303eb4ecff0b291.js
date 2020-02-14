8651e5a4eae647d3ca7ce5b575efb7b1fc611e7e
James Hagood <cardfaux@tutanota.com>
2020-02-14 13:53:49 -0600

    aded node server

---
+++ build/precache-manifest.fafd4eabec4aa8fe7303eb4ecff0b291.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "129baa4be16d0bc49fb544dd31aa8320",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38de7700ba00409cc5d8",
+    "url": "/static/css/main.b34273fd.chunk.css"
+  },
+  {
+    "revision": "892fdd0fac0a19b5a101",
+    "url": "/static/js/2.2e4c45a9.chunk.js"
+  },
+  {
+    "revision": "170392c3acad6bbffec743442b1d0ac3",
+    "url": "/static/js/2.2e4c45a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38de7700ba00409cc5d8",
+    "url": "/static/js/main.951fce62.chunk.js"
+  },
+  {
+    "revision": "fb2b365f3d345041b229",
+    "url": "/static/js/runtime-main.efc2974b.js"
+  }
+]);
